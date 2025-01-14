d0d7907ef5b8c3b995c8800caf3d3a5c59deb506
Daniel Monterde <dn.monterde@gmail.com>
2025-01-14 22:40:29 +0800

    Added dashboard

--- KafkaToolUI/src/app/modules/dashboard/dashboard.component.ts
+++ KafkaToolUI/src/app/modules/dashboard/dashboard.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule, MatGridListModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
