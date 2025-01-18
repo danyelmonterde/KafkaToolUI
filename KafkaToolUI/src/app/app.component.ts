9d72e21d1759c65aa44a0d36093f934e107f3146
jembalatinsayo <jembalatinsayo@gmail.com>
2025-01-18 20:22:21 +0800

    [App] Added imports and added dashboard/new in routing

--- KafkaToolUI/src/app/app.component.ts
+++ KafkaToolUI/src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router'
-import { NavComponent } from "./components/nav/nav.component";
-
+import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
+import { MaterialModule } from './shared/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent,],
+  imports: [
+    RouterOutlet,
+    NavComponent,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'KafkaToolUI';
--- KafkaToolUI/src/app/app.routes.ts
+++ KafkaToolUI/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegisterKafkaConnectionComponent } from './modules/register-kafka-connection/register-kafka-connection.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    { path: 'dashboard', component: DashboardComponent } 
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard/new', component: RegisterKafkaConnectionComponent },
 ];
