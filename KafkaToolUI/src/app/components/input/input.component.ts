3f2b10a0953f5fe10dbb0c54d79337a6da828c88
jembalatinsayo <jembalatinsayo@gmail.com>
2025-01-19 19:30:20 +0800

    [Input] Updated conditions for displaying inputs

--- KafkaToolUI/src/app/components/input/input.component.ts
+++ KafkaToolUI/src/app/components/input/input.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MaterialModule } from '../../shared/material.module';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -13,4 +13,18 @@ import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 export class InputComponent {
   @Input() formGroup!: FormGroup;
   @Input() inputs!: any[]; // Array of form fields and their properties
+  @Input() toggleValue = false; // Receive value from parent
+  @Input() labelPosition: 'before' | 'after' = 'after'; // Optional customizations
+  @Input() color: 'primary' | 'accent' | 'warn' = 'primary';
+
+  @Output() valueChange = new EventEmitter<boolean>(); // Emit changes to parent
+
+  onToggleChange(newValue: boolean) {
+    this.toggleValue = newValue; // Update the child’s toggleValue
+    this.valueChange.emit(newValue); // Notify parent
+  }
+
+  trackByFn(index: number, item: any): number {
+    return index; // Track by index to optimize rendering
+  }
 }
