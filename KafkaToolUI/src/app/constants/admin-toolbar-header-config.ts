649b655a6128a0a64ad52bbde39309d8c350ca19
jembalatinsayo <jembalatinsayo@gmail.com>
2025-02-08 18:56:04 +0800

    [Cnstnts - ATHC] Renamed file and added config for other routes

--- KafkaToolUI/src/app/constants/admin-toolbar-header-config.ts
+++ KafkaToolUI/src/app/constants/admin-toolbar-header-config.ts
@@ -44,4 +44,38 @@ export const CREATE_BTN_CONFIG: {
       color: '',
     },
   ],
+  '/admin/cluster/view': [
+    {
+      tooltip: 'Save service configuration',
+      routerLink: '/admin/cluster/new',
+      icon: 'edit',
+      label: 'Edit configuration',
+      disabled: false, // Dynamically set in the component
+      color: '#ff9100',
+    },
+    {
+      tooltip: 'Verify connection to the cluster',
+      routerLink: '/admin/cluster/view',
+      icon: 'check',
+      label: 'Verify',
+      disabled: false, // Dynamically set in the component
+      color: '#69f0ae',
+    },
+    {
+      tooltip: 'Delete cluster registration',
+      routerLink: '/admin/cluster',
+      icon: 'close',
+      label: 'Delete Connection',
+      disabled: false,
+      color: '#f44336',
+    },
+    {
+      tooltip: 'Navigate to Schema Register page',
+      routerLink: '/admin/cluster/new',
+      icon: 'settings',
+      label: 'Register New',
+      disabled: false,
+      color: '#69f0ae',
+    },
+  ],
 };
