9ebb723afc1a6603a111b27f0f7a51e04d37720a
jembalatinsayo <jembalatinsayo@gmail.com>
2025-02-08 18:56:51 +0800

    [Svcs - API] Created function for headers

--- KafkaToolUI/src/app/core/services/api.service.ts
+++ KafkaToolUI/src/app/core/services/api.service.ts
@@ -1,14 +1,31 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environments';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  private apiUrl = 'https://api.example.com/items'; // Replace with your API URL
+  private apiUrl = `${environment.apiBaseUrl}/broker`; // API URL
+  private token = 'eyJhbGciOiJIUzUxMiJ9...'; // Replace with your actual token
 
   constructor(private http: HttpClient) {}
+
+  // Function to set headers with Authorization token
+  private getHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.token}`,
+    });
+  }
+
+  // Save (POST) a new item
+  saveItem(endpoint: string, data: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/${endpoint}`, data, {
+      headers: this.getHeaders(),
+    });
+  }
 
   // GET all items
   getItems(): Observable<any[]> {
